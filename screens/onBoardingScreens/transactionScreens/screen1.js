8011aec32141c5e7cc36966c6c3a65a38edb1a12
Hachem Zarrad <hachemzarrad@gmail.com>
2021-06-14 02:45:26 +0100

    First Transaction Screen

--- screens/onBoardingScreens/transactionScreens/screen1.js
+++ screens/onBoardingScreens/transactionScreens/screen1.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import Paragraph from '../../../components/paragraph'
+import CustomImage from '../../../components/customImage'
+
 
 const Screen1 = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <View style={styles.comics}>
+                <CustomImage source={require('../../../assets/comic1.jpg')} />
+                <CustomImage source={require('../../../assets/comic2.jpg')} />
+                <CustomImage source={require('../../../assets/comic3.jpg')} />
+            </View>
+            <Paragraph text='Enjoy hundreds of comics, posters, pictures and fascinating stories' />
         </View>
+
     )
 }
 
 export default Screen1
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    comics: {
+        flexDirection: 'row'
+    }
+})
