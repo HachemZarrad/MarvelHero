7625cc9bb293f2bcd8f715214e5266d8ffb87e1e
Hachem Zarrad <hachemzarrad@gmail.com>
2021-06-14 02:57:41 +0100

    Second Transaction Screen

--- screens/onBoardingScreens/transactionScreens/screen2.js
+++ screens/onBoardingScreens/transactionScreens/screen2.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Paragraph from '../../../components/paragraph'
+
 
 const Screen2 = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <View style={styles.imageFrame}>
+                <Image
+                    source={require('../../../assets/blackSpider.jpg')}
+                    style={styles.image} />
+            </View>
+            <Paragraph text='Get deep into the world of your favorite heroes and their epic fights.'/>
         </View>
     )
 }
 
 export default Screen2
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    image: {
+        borderRadius: 20,
+        height: 220,
+        width: 190
+    },
+    imageFrame: {
+        
+    }
+})
