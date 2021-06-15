db23384eee79964303bde63e98b2927df05decf5
Hachem Zarrad <hachemzarrad@gmail.com>
2021-06-15 03:50:17 +0100

    Get Favorite Hero Action

--- redux/actions/actionsTypes.js
+++ redux/actions/actionsTypes.js
@@ -1,2 +1,3 @@
 export const GET_HEROES = 'GET_HEROES'
+export const GET_FAV_HERO = 'GET_FAV_HERO'
 export const GET_COMICS = 'GET_COMICS'
--- redux/actions/heroes.js
+++ redux/actions/heroes.js
@@ -2,13 +2,17 @@ import * as ActionTypes from './actionsTypes'
 import { baseUrl, apiKey, hash, ts } from '../../constants/networking'
 
 export const fetchHeroes = () => async (dispatch) => {
-        try {
-            const response = await fetch(`${baseUrl}:443/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}`)
-            if (!response.ok) throw new Error('Please Check Your Internet Connection')
-            const heroes = await response.json()
-            dispatch({ type: ActionTypes.GET_HEROES ,payload: heroes })
-        }
-        catch (error) {
-            throw error
-        }
+    try {
+        const response = await fetch(`${baseUrl}:443/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}`)
+        if (!response.ok) throw new Error('Please Check Your Internet Connection')
+        const heroes = await response.json()
+        dispatch({ type: ActionTypes.GET_HEROES, payload: heroes })
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+export const getFavoriteHero = (heroId) => {
+    dispatch({ type: ActionTypes.GET_FAV_HERO, payload: heroId })
 }
