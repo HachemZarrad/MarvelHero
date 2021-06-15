3d14c35b0bc0efc476b8fc1a6a01f423b3ae0147
Hachem Zarrad <hachemzarrad@gmail.com>
2021-06-15 03:36:46 +0100

    Getting Comics Action

---
+++ redux/actions/comics.js
@@ -0,0 +1,14 @@
+import * as ActionTypes from './actionsTypes'
+import { baseUrl, apiKey, hash, ts } from '../../constants/networking'
+
+export const fetchComics = (heroId) => async (dispatch) => {
+        try {
+            const response = await fetch(`${baseUrl}/v1/public/characters/${heroId}/comics?ts=${ts}&apikey=${apiKey}&hash=${hash}`)
+            if (!response.ok) throw new Error('Please Check Your Internet Connection')
+            const comics = await response.json()
+            dispatch({ type: ActionTypes.GET_COMICS, payload: comics })
+        }
+        catch (error) {
+            throw error
+        }
+}
