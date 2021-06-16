18d7e3fd1d2ee12140fefdcb921ceeaa4caf6c72
Hachem Zarrad <hachemzarrad@gmail.com>
2021-06-16 23:21:46 +0100

    final version

--- constants/colors.js
+++ constants/colors.js
@@ -1,9 +1,6 @@
 export default {
     background: 'rgba(52, 52, 52, 0.8)',
-    primary: '#C2185B',
     accent: '#FFC107',
     frame: '#778899',
     toolbarColor: '#46618e',
-    buttonContainer: '#343b45',
-    button: '#3e86ed',
 };
